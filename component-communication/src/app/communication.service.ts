0e80a872885b2168c52273d96fca0acb5c3c2a11
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-12 11:08:43 +0100

    cleaned code

--- component-communication/src/app/communication.service.ts
+++ component-communication/src/app/communication.service.ts
@@ -9,18 +9,10 @@ export class ComumnicationService {
  public observableMessage: BehaviorSubject<string> = new BehaviorSubject<string>(
     'parent using observable prop'
   );
-messageParent$ = this.observableMessage.asObservable();
-
-messageObservableParent() {
-  return this.observableMessage.next('parent using observable prop')
-}
+msgObservableParent$ = this.observableMessage.asObservable();
 
   constructor() {}
 
   //SERVICE PROP:
   serviceParentMessage = new EventEmitter<string>();
 }
-
-/*
-https://www.youtube.com/watch?v=W1nQXZwXKb0
-*/
