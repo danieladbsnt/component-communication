e09714d6ef0ba6c781716ee838006b52d493d31b
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-16 12:59:36 +0100

    modified observable

--- component-communication/src/app/communication.service.ts
+++ component-communication/src/app/communication.service.ts
@@ -1,16 +1,17 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ComumnicationService {
   //OBSERVABLE PROP:
- public observableMessage: BehaviorSubject<string> = new BehaviorSubject<string>(
-    'parent using observable prop'
-  );
+ public observableMessage: BehaviorSubject<string> = new BehaviorSubject<string>('');
 msgObservableParent$ = this.observableMessage.asObservable();
 
+setMsgParentChild() {
+  this.observableMessage.next('parent using observable prop')
+}
   constructor() {}
 
   //SERVICE PROP:
